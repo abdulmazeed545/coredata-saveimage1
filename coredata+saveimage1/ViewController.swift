e451c7a9edfe22ac1f3de4426cb42b06500c8f0e
mazeed <abdulmazeedshaik45@gmail.com>
2021-04-16 11:50:59 +0530

    Initial Commit

---
+++ coredata+saveimage1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  coredata+saveimage1
+//
+//  Created by Shaik abdul mazeed on 16/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
